74c3f3e02b8a80f822c45190cff139176ae9e79d
A. <Alekseew.zheka0207@gmail.com>
2024-11-29 14:23:41 +0200

    Add project files.

---
+++ rest of tasks/Palindrome.cpp
@@ -0,0 +1,1 @@
+#include "Palindrome.h"
---
+++ rest of tasks/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Palindrome.h"
+using namespace std;
+
+int main() {
+	Palindrome some_pal("citticat12321tacittic");
+	cout << some_pal;
+
+
+	return 0;
+}
