377b0e957c17f86d97bc79ce0677cec700cbcbef
A. <Alekseew.zheka0207@gmail.com>
2024-11-29 15:02:00 +0200

    Letters

--- rest of tasks/main.cpp
+++ rest of tasks/main.cpp
@@ -2,12 +2,16 @@
 #include <vector>
 #include <random>
 #include <chrono>
+#include <Windows.h>
 
 #include "Palindrome.h"
 
 using namespace std;
 
 int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
 	Palindrome some_pal("citticat12321tacittic");
 	cout << some_pal << '\n' << '\n';
 
@@ -29,5 +33,26 @@ int main() {
 #pragma endregion /*find_max*/
 	cout << '\n' << min;
 
+#pragma region counting letters
+    string sentence = "Write a program that counts the number of vowels and consonants in a given English text.";
+    int vowel_letters = 0;
+    int consonant_letters = 0;
+
+    for (int i = 0; i < sentence.size(); i++) {
+        char ch = sentence[i];
+		if (ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' ||
+            ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o' ||
+            ch == 'U' || ch == 'u') {
+            vowel_letters++;
+        }
+		else if(isalpha(ch)){
+            consonant_letters++;
+        }
+    }
+#pragma endregion /*counting letters*/
+
+    cout << "Text: " << sentence << endl;
+    cout << "Vowels: " << vowel_letters << endl;
+    cout << "Consonants: " << consonant_letters << endl;
 	return 0;
 }
